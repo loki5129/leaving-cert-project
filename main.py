713678096a8b2fc9b8fa14cd8549b4a8342ccc52
loki5129 <123837505+loki5129@users.noreply.github.com>
2024-12-04 14:24:09 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from pandas import *
 #csv file name
-filename = "C:\\Users\\Student 2021\\Desktop\\tsw\\CS PROJECT\\Primary-energy-by-fuel.csv"
+filename = "C:\\Users\\Student 2021\\Desktop\\tsw\\CS PROJECT\\Pokemon.csv"
 
 
 data = read_csv(filename)
 
-years = data["Primary energy by fuel"].tolist()
-oil = data["Oil"].tolist()
-renewable = data["Renewables"].tolist()
-gas = data["Gas"].tolist()
-coal = data["Coal"].tolist()
-print(years)
-print(oil)
+ids = data["trainerID"].tolist()
+name= data["name"].tolist()
+
+
+
+# zip the two lists together to create a list of key-value pairs
+key_value_pairs = zip(ids, name)
+
+# convert the list of key-value pairs to a dictionary
+data = dict(key_value_pairs)
+
+keys = list(data.keys())
+
+for i in range(len(data)):
+ with open("test.txt","a+") as f :
+  f.write("\n"+ keys[i],data[i])
+  
+#print(keys)

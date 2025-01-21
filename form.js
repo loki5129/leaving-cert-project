4d86d4ef1330726756558a83b3e4287169acd4e3
loki5129 <123837505+loki5129@users.noreply.github.com>
2025-01-21 15:06:29 +0000

    Add files via upload

---
+++ form.js
@@ -0,0 +1,24 @@
+function $_GET(q, s = window.location.search) {
+  const params = new URLSearchParams(s);
+  return params.has(q) ? params.get(q) || '' : undefined;
+}
+var usersName = $_GET('username');
+//console.log(usersName)
+
+
+
+if(typeof(usersName)=='string'){
+  
+    if (typeof(age)=="intger"){
+    
+    
+    }
+     if (typeof(like)=="boolean"){
+        
+      }
+  
+
+      
+    }
+
+

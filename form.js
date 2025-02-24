de1d22210ab9b17b1bb92d99fc0554eb68df0970
loki5129 <123837505+loki5129@users.noreply.github.com>
2025-02-24 21:25:59 +0000

    Add files via upload

--- form.js
+++ form.js
@@ -1,5 +1,54 @@
-let userdata = [["loki5129",18,true]];
+initialize_database()
+main_user_update()
+function main_user_update(){
+ let users=JSON.parse(localStorage.getItem("userdata"))
+ let main_user_name = "loki5129"
+ let main_user_age = 18
+ let main_user_state =true
+ let  main_user_check = users.some(user => user.name === main_user_name)
+ if (main_user_check){
+ 
+ } else{
+  add_data(main_user_name,main_user_age,main_user_state)
+ }
+}
 
+
+
+function local_test() {
+ 
+  localStorage.setItem("data",[["name","23","true"]]);
+  let test2 = localStorage.getItem("data")
+  console.log(test2)
+ }
+
+function initialize_database(){
+  if (!localStorage.getItem("userdata")){
+    localStorage.setItem("userdata",JSON.stringify([]));
+  }
+}
+
+function add_data(name,age,state){
+  let users = JSON.parse(localStorage.getItem("userdata"))
+  let new_user ={
+    name: name,
+    age: age,
+    state: state
+  };
+  let username_exist=users.some(user => user.name === name);
+  if (username_exist){
+    alert("username invaild");
+  } else{
+  users.push(new_user);
+  localStorage.setItem("userdata",JSON.stringify(users))
+}
+}
+function clear_database(){
+  localStorage.clear()
+  initialize_database()
+  main_user_update()
+  clear_table()
+}
 function submitData() {
  let userdata1 = []
  let username = document.getElementById('username').value;
@@ -25,60 +74,86 @@ function submitData() {
  }else {
   userdata1.push(checkboxes[0].value === "True" ? true : false);
   }
-
-  userdata.push([...userdata1]);
+  state = userdata1[2]
+  add_data(username,age,state)
 
  }
-
+function full_record(){
+  let users=JSON.parse(localStorage.getItem("userdata"))
+  console.log(users)
+}
 
 function test(){
-  alert(userdata[1][2])}
-
+  //alert(userdata[1][2])}
+  console.log(typeof(JSON.parse(localStorage.getItem("userdata"))))
+}
 function table(){
- //if (userdata[1]===undefined||userdata[1]===null||userdata[1]===""|| typeof(userdata[1])===undefined){
- console.log("userdata[1]:", userdata[1]);
- if (!userdata[1]|| userdata[1]==""|| userdata[1]==[]){
-   alert("missing required data")
-   return;
- }
-  var x = document.createElement("table")
-  x.setAttribute("id","mytable");
-  document.body.appendChild(x);
+  let users=JSON.parse(localStorage.getItem("userdata"));
+  
+  if (!users||users.length === 0){
+    alert("no users found");
+    return;
+   } 
+  table = document.getElementById("table")
+  if (!table){
+    var table = document.createElement("table");
+    table.setAttribute("id", "table");
 
-  var y = document.createElement("TR");
-  y.setAttribute("id", "myTr");
-  document.getElementById("mytable").appendChild(y);
 
-  var z = document.createElement("TD");
-  var p = document.createTextNode("1");
-  z.appendChild(p)
-  document.getElementById("myTr").appendChild(z);
+    let header_row = document.createElement("tr");
+    let headers =["user id","username","age","state"]
+    
+    headers.forEach(header => {
+      let th = document.createElement("TH");
+      let header_text = document.createTextNode(header);
+      th.appendChild(header_text);
+      header_row.appendChild(th);
+    });
 
-  var z = document.createElement("TD");
-  var t = document.createTextNode(userdata[0]);
-  z.appendChild(t);
-  document.getElementById("myTr").appendChild(z);
-
+    table.appendChild(header_row);
+    document.body.appendChild(table);
+    
+  }
 
   
-  var y = document.createElement("TR");
-  y.setAttribute("id", "myTr2");
-  document.getElementById("mytable").appendChild(y);
-
-  var z = document.createElement("TD");
-  var p = document.createTextNode("2");
-  z.appendChild(p)
-  document.getElementById("myTr2").appendChild(z);
-
-  var k = document.createElement("TD");
-  var r = document.createTextNode(userdata[1]);
-  k.appendChild(r);
-  document.getElementById("myTr2").appendChild(k);
+   let row_count =  table.getElementsByTagName("tr").length - 1;
+  
+ 
+  for (let i =row_count;i< users.length; i++){
+   let user = users[i];
+   if (!user){
+    alert("missing required data for user " +i);
+    continue;
+    } 
+    let row_there = document.getElementById("tr" + i);
+    if (row_there){
+      continue;
+    }
+    let row =document.createElement("Tr");
+    row.id = "mytr"+ i;
+    
+    let cell = document.createElement("TD");
+    let text = document.createTextNode(i + 1);
+    cell.appendChild(text);
+    row.appendChild(cell);
+    let data = [user.name,user.age,user.state];
+    data.forEach(element =>{
+      let cell = document.createElement("TD")
+      let text = document.createTextNode(element);
+      cell.appendChild(text),
+      row.appendChild(cell)
+    });
+   table.appendChild(row)
+  }
+}
+function clear_table(){
+ const table = document.getElementById("table");
+ table.remove();
 }
 
 
 function analysis() {
-  if(userdata[1] == null||userdata[1]=="" ||userdata[1]==undefined|| typeof(userdata[1])==undefined){
+  /*if(userdata[1] == null||userdata[1]=="" ||userdata[1]==undefined|| typeof(userdata[1])==undefined){
     alert("missing requird data")
   }else{
    if (userdata[1][2]===false) {
@@ -97,6 +172,6 @@ function analysis() {
     document.getElementById("result").innerHTML = result
     document.getElementById("analysis_button").disabled = true;
   }
-  }
+  }*/
 }
 
---
+++ torun.bat
@@ -0,0 +1,5 @@
+eccho@ off
+title "leaving cert project"
+py main.py
+start http://localhost:8000
+python -m http.server 8000  

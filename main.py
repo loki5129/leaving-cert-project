2b4a1ac74755a1684ef68acf377256dd9437d342
loki5129 <123837505+loki5129@users.noreply.github.com>
2025-03-06 12:49:18 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ oginal_filename = r"Pokemon.csv"
 #section  of code resolving cleaning the csv file
 raw_data = read_csv(oginal_filename)
 
- 
+ #removes all column with the name in the list
 to_remove=["place","type2","hp","maxhp","attack","defense","spatk","spdef","speed"]
- 
 raw_data.drop(columns=to_remove,inplace=True)
+
+#wirtes cleaned to a new csv
 raw_data.to_csv("cleaned_data.csv",index=False)
 filename = "cleaned_data.csv"
 
+
+#reads new csv and turns it into a list of rows
 data = read_csv(filename)
-
-
 data_dict = data.to_dict(orient='records')
 
+#creates empty list for levels and empty dict for types
 level_list = []
 types = {
     "Water" :0,
@@ -42,7 +44,7 @@ types = {
     "Normal":0,
 
 }
-
+#counts how many times each types is in the data and writes it to the corresponping dict key
 keys = list(types.keys())
 for i in range(len(data_dict)):
     for c in range(len(keys)):
@@ -53,7 +55,8 @@ for i in range(len(data_dict)):
 #print(types)
 
 def clean(string):
-  
+  #function to get the level out of the index of the list of data
+  #uses varouis string method and re method to remove all letters extra numbers and specail characters
   string=str(string)
   string=string.lower()
   cleaned =re.sub(r"[\([{})\]]","",string)
@@ -78,6 +81,7 @@ def clean(string):
   #print(cleaned)
   return cleaned
 
+#applys the clean function to extract the level and apppend it to the level_list
 for i in range(len(data_dict)):
     level_to_be_added=clean(data_dict[i])
     #print(level_to_be_added)
@@ -85,22 +89,27 @@ for i in range(len(data_dict)):
 
 
 
-
+#defines colors for each bar in the bar chart
 color = ["#1552E2","#AB2021","#147B3D","#A9702C","#48180B","#1C4B27","#4A677D","#E3E32B","#86D2F5","#5F756D","#5E2D88","#448B95","#33336B","#040706","#971944","#994025","#A42A6C","#FAF9F6"]#defien each colour for each bar in hexcode
 
-
+#gets the x of the bar chart to be the the name of each type
+#get the y as the correspong amount of time that tpye appears in the data
 bar_chart_y= []
 bar_chart_x=[]
 for i in range(len(keys)):
     bar_chart_y.append(types[keys[i]])
     bar_chart_x.append(keys[i])
 
+
+#get the mean meadian range and mode for the count of types in the data
 stat_type_list ={
     "mean":mean(bar_chart_y),
     "median":median(bar_chart_y),
     "range":max(bar_chart_y)-min(bar_chart_y),
     "mode":mode(bar_chart_y)
 }
+
+#gets the mean meadian range and mode for the levels in the data
 stat_level_list ={
     "mean":mean(level_list),
     "median":median(level_list),
@@ -111,7 +120,7 @@ stat_level_list ={
 
 
 
-
+#creates the bar chart bar with a black outline and 
 bar_chart=go.Bar(
     x=bar_chart_x 
     ,y=bar_chart_y,
